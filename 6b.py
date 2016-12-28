b35540d657de2ab5ed05ae9af9c3cb7b06e7fe6c
Dinesh Manajipet <saidinesh5@gmail.com>
2016-12-28 22:50:14 +0530

    Day 6

---
+++ 6b.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+def get_columns(lines):
+    columns = []
+
+    for row in lines:
+        for column in range(len(row)):
+            if column >= len(columns):
+                columns.append([])
+            columns[column].append(row[column])
+
+    return columns
+
+def most_common_character(line):
+    frequency = defaultdict(int)
+    for c in line:
+        frequency[c] += 1
+    return min(frequency.items(), key = lambda x: x[1])[0]
+
+def process_input(lines):
+    return ''.join(list(map(lambda column: most_common_character(column), get_columns(lines))))
+
+def main():
+    #lines = ['eedadn', 'drvtee', 'eandsr', 'raavrd', 'atevrs', 'tsrnev', 'sdttsa', 'rasrtv', 'nssdts', 'ntnada', 'svetve', 'tesnvt', 'vntsnd', 'vrdear', 'dvrsen', 'enarar']
+    lines = open('input6.txt').read().strip().split('\n')
+    result = process_input(lines)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
